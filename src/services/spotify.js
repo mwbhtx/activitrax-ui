891fcb687d3158a6c342ae81c8116c8df1b0b306
Matthew <mwbhtx@outlook.com>
2026-01-12 12:49:25 -0700

    feat(ui): add spotify playlist service functions and extended scopes

--- src/services/spotify.js
+++ src/services/spotify.js
@@ -20,4 +20,42 @@ export const exchangeSpotifyAuthToken = async (api_token, auth_token, scopes) =>
     return response.data
 }
 
-export const spotify_scopes = 'user-read-recently-played user-read-email user-read-private'
+export const getSpotifyPlaylists = async (api_token) => {
+    const reqConfig = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + api_token
+        },
+        url: process.env.REACT_APP_ACTIVITRAX_API_URL + "/spotify/playlists"
+    }
+
+    const response = await axios(reqConfig);
+    return response.data
+}
+
+export const addTrackToPlaylist = async (api_token, playlistId, trackUri) => {
+    const reqConfig = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + api_token
+        },
+        url: process.env.REACT_APP_ACTIVITRAX_API_URL + `/spotify/playlists/${playlistId}/tracks`,
+        data: {
+            track_uri: trackUri
+        }
+    }
+
+    const response = await axios(reqConfig);
+    return response.data
+}
+
+// Base scopes (existing functionality)
+export const spotify_scopes_base = 'user-read-recently-played user-read-email user-read-private'
+
+// Full scopes including playlist modification
+export const spotify_scopes_with_playlists = 'user-read-recently-played user-read-email user-read-private playlist-modify-public playlist-modify-private'
+
+// Default scopes to request (now includes playlist permissions)
+export const spotify_scopes = spotify_scopes_with_playlists
